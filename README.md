9c0d61e9ef31f705e9b0be57c82c848e638898c3
twekberg <tekberg@gmail.com>
2024-04-19 18:38:26 -0700

    Added code to read JSON data from a subreddit

--- README.md
+++ README.md
@@ -6,12 +6,17 @@ Stored already view in sqlite database.
 Notes:
 
 Start the server with:
+  cd ~/src/red
   python -m http.server --cgi
 
 Run hello.py in cgi-bin with this in a browser:
   http://localhost:8000/cgi-bin/hello.py
 it displays 'Hello, World!'
+
+Run red.lpy in cgi-bin with this in a browser:
   http://localhost:8000/cgi-bin/red.py
+It will display a list of titles as links. Click on the link to see an
+  image or video.
 
 
 TODO::
@@ -23,4 +28,24 @@ When setting up a file for display, convert the last part of the url to lower ca
 Use last part in file_list to see if I already have downloaded it, and don't display.
 
  os.chdir('F:/N/O/SPELLSNO/IMAGES/xxxbunker')
- 
+-------------------------------------------------------------------------------
+Load simple.html in a browser. Click the reddit link. After a few
+seconds the image will appear.
+-------------------------------------------------------------------------------
+This is a useful guide to extracting reddit postings in JSON format
+
+https://www.jcchouinard.com/documentation-on-reddit-apis-json/
+
+This link:
+
+https://www.reddit.com/r/python/top.json?limit=100&t=month
+
+in a browser will display the JSON file in a raw or formatted manner.
+Use this URL to display JSON data for FromBelowView
+
+https://www.reddit.com/r/FromBelowView/top.json?limit=100&t=month
+
+listing = 'top' # controversial, best, hot, new, random, rising, top
+timeframe = 'month' #hour, day, week, month, year, all
+
+The above URL uses a listing of top and a timeframe of month.
---
+++ get_json.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+"""
+Read JSON data from a specific subreddit.
+Writes the formatted JSON to stdout.
+"""
+
+
+import argparse
+import json
+import sys
+import urllib.request
+
+
+def build_parser():
+    """
+    Collect command line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('subreddit',
+                        help='The name of the subreddit. ')
+    parser.add_argument('-t', '--timeframe',
+                        choices=['hour', 'day', 'week', 'month', 'year', 'all'],
+                        default='day',
+                        help='Amount of time to look at. '
+                        ' Default: %(default)s.')
+    parser.add_argument('--limit',
+                        default=100,
+                        help='Limit on the number of postings to retrieve. '
+                        ' Default: %(default)s.')
+
+    parser.add_argument('-l', '--listing',
+                        choices=['controversial', 'best', 'hot', 'new',
+                                 'random', 'rising', 'top'],
+                        default='new',
+                        help='Sort articles according to this. '
+                        ' Default: %(default)s.')
+
+    return parser
+
+
+def main(args):
+    """
+    Retrieve JSON data.
+    """
+    url = f'https://www.reddit.com/r/{args.subreddit}/' \
+          f'{args.listing}.json?limit={args.limit}&t={args.timeframe}'
+    print(f'{url=}')
+    fp = urllib.request.urlopen(url)
+    json_bytes = fp.read()
+    json_data = json.loads(json_bytes.decode("utf8", errors='ignore'))
+    fp.close()
+    sys.stdout.reconfigure(errors='ignore')
+    print(json.dumps(json_data, sort_keys=True,
+                     indent=4, separators=(',', ': ')))
+
+
+if __name__ == '__main__':
+    sys.exit(main(build_parser().parse_args()))
